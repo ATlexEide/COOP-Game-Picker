83b4508d94f39980d6cf627a9d84a6564172ca12
Alexander Eide <atlexeide@gmail.com>
2025-07-31 15:50:29 +0200

    Add setError

--- src/utils/getAndSetUserLibraries.js
+++ src/utils/getAndSetUserLibraries.js
@@ -1,6 +1,7 @@
 import { getUserLibrary } from "./getUserLibrary";
 
 export default async function getAndSetUserLibraries(
+  setError,
   setStatus,
   setLibrary,
   ...users
@@ -11,9 +12,9 @@ export default async function getAndSetUserLibraries(
 
   setStatus("Getting libraries");
   console.log("Getting libraries");
-  const lib1 = await getUserLibrary(users[0].steamid);
+  const lib1 = await getUserLibrary(setError, users[0].steamid);
   if (!lib1 || !lib1.length) throw new Error("Couldnt get user1 library");
-  const lib2 = await getUserLibrary(users[1].steamid);
+  const lib2 = await getUserLibrary(setError, users[1].steamid);
   if (!lib2 || !lib2.length) throw new Error("Couldnt get user2 library");
   if (lib1 && lib2) setLibrary([lib1, lib2]);
 }
