808b28482e83b6baee02f953b4d3b484755a5b9a
Alexander Eide <atlexeide@gmail.com>
2025-07-31 15:51:53 +0200

    Add setStatus and setError

--- src/utils/getGameDetails.js
+++ src/utils/getGameDetails.js
@@ -1,4 +1,12 @@
-export default async function getGameDetails(appID) {
+export default async function getGameDetails(
+  setStatus,
+  setError,
+  currentGame,
+  totalGames,
+  appID
+) {
+  setStatus(`Getting game details [${currentGame}/${totalGames}]`);
+  console.log("Getting game details");
   try {
     const req = await fetch(
       `http://undecidedgamespinnerserver-production.up.railway.app/game/${appID}`
@@ -6,6 +14,7 @@ export default async function getGameDetails(appID) {
     const res = await req.json();
     return res[appID].data;
   } catch (error) {
+    setError(error);
     console.log(error);
   }
 }
