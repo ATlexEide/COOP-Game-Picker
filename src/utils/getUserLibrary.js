c9c15dc0d713b116030dd9d43b9511e151fc6766
Alexander Eide <atlexeide@gmail.com>
2025-07-31 15:52:13 +0200

    Add setError

--- src/utils/getUserLibrary.js
+++ src/utils/getUserLibrary.js
@@ -1,7 +1,11 @@
-export async function getUserLibrary(steamID) {
+export async function getUserLibrary(setError, steamID) {
   return await fetch(
     `https://undecidedgamespinnerserver-production.up.railway.app/library/${steamID}`
   )
     .then((res) => res.json())
-    .then((res) => res.response.games);
+    .then((res) => res.response.games)
+    .catch((err) => {
+      setError(err);
+      throw new Error(err);
+    });
 }
