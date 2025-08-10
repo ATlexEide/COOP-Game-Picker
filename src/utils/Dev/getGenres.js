82c93f1bf749b7351e8c79290708a7b26d2fcb71
Alexander Eide <atlexeide@gmail.com>
2025-08-10 15:43:11 +0200

    Fix function not returning array of genres

--- src/utils/Dev/getGenres.js
+++ src/utils/Dev/getGenres.js
@@ -1,14 +1,16 @@
 import { sampleGames } from "../../data";
 
 export function getGenres() {
+  const arr = [];
   let currGenreID = 0;
   while (currGenreID < 100) {
-    checkGenres(currGenreID);
+    checkGenres(currGenreID, arr);
     currGenreID++;
   }
+  return arr;
 }
 
-function checkGenres(i) {
+function checkGenres(i, array) {
   const hasGenre = (genre) => genre.id == i;
   let isFound = false;
   let genreName = "";
@@ -21,8 +23,5 @@ function checkGenres(i) {
       isFound = true;
     }
   });
-  console.log(i);
-  if (!isFound) return;
-  if (isFound) console.log("ID", i, "DESCRIPTION", genreName);
+  if (genreName) array.push({ id: i, description: genreName });
 }
-////////////
