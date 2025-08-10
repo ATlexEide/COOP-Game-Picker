450e7a7aa7df20625e5f03764005ac0f37196f36
Alexander Eide <atlexeide@gmail.com>
2025-08-10 21:34:04 +0200

    .

--- src/utils/Dev/getDetailedLibrary.js
+++ src/utils/Dev/getDetailedLibrary.js
@@ -3,16 +3,29 @@ import getGameDetails from "../getGameDetails";
 
 export async function getDetailedLibrary(userID, loggingEnabled = false) {
   const library = await getUserLibrary(userID);
+  console.log(library);
   const detailedLibrary = [];
-  for (let i = 0; i < library.length - 1; i++) {
+  for (let i = 0; i < library?.length - 1; i++) {
     if (loggingEnabled) {
-      console.clear();
+      // console.clear();
       console.log(`${i} / ${library.length}`);
       console.log(library[i]);
     }
     const game = await getGameDetails(library[i].appid);
-    detailedLibrary.push(game);
+    switch (game) {
+      case "429":
+        console.log("Failed to fetch, trying again in 60 seconds");
+        setTimeout(() => {
+          console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        }, 1000);
+        break;
+
+      default:
+        detailedLibrary.push(game);
+        break;
+    }
   }
+  if (loggingEnabled) console.log(library);
   if (loggingEnabled) console.log(detailedLibrary);
   return detailedLibrary;
 }
