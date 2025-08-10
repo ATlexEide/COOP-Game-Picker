7b54639cf0590309ebb65eb8614dc93b98ea9823
Alexander Eide <atlexeide@gmail.com>
2025-08-10 20:36:31 +0200

    Fix fetch url

--- src/utils/getUserLibrary.js
+++ src/utils/getUserLibrary.js
@@ -1,6 +1,6 @@
 export async function getUserLibrary(steamID, setError) {
   return await fetch(
-    `https://undecidedgamespinnerserver-production.up.railway.app/library/${steamID}`
+    `http://undecidedgamespinnerserver-production.up.railway.app/library/${steamID}`
   )
     .then((res) => res.json())
     .then((res) => res.response.games)
--- src/utils/getUserProfile.js
+++ src/utils/getUserProfile.js
@@ -1,6 +1,6 @@
 export async function getUserProfile(steamID, setUser) {
   await fetch(
-    `https://undecidedgamespinnerserver-production.up.railway.app/user/${steamID}`
+    `http://undecidedgamespinnerserver-production.up.railway.app/user/${steamID}`
   )
     .then((res) => res.json())
     .then((res) => res.response.players[0])
