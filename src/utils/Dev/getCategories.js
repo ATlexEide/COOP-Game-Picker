dd53a826f61761452c83c60b80cccc148efd8cca
Alexander Eide <atlexeide@gmail.com>
2025-08-10 15:41:32 +0200

    Add function to get categories

---
+++ src/utils/Dev/getCategories.js
@@ -0,0 +1,27 @@
+import { sampleGames } from "../../data";
+
+export function getCategories() {
+  const arr = [];
+  let currCatID = 0;
+  while (currCatID < 100) {
+    checkCategoryID(currCatID, arr);
+    currCatID++;
+  }
+  return arr;
+}
+
+function checkCategoryID(i, array) {
+  const hasCat = (cat) => cat.id === i;
+  let isFound = false;
+  let catName = "";
+  sampleGames.forEach((game) => {
+    if (isFound) return;
+    if (!game) return;
+    if (!game.categories) return;
+    if (game.categories.some(hasCat)) {
+      catName = game.categories.find((obj) => obj.id === i).description;
+      isFound = true;
+    }
+  });
+  if (catName) array.push({ id: i, description: catName });
+}
