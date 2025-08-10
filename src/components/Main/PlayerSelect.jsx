46526a537f87bab0aa66e61979e87c55a496f8da
Alexander Eide <atlexeide@gmail.com>
2025-08-10 23:48:48 +0200

    Update user select component

--- src/components/Main/PlayerSelect.jsx
+++ src/components/Main/PlayerSelect.jsx
@@ -1,13 +1,19 @@
-export default function PlayerSelect({ text, setUserId }) {
+import { useState } from "react";
+
+export default function PlayerSelect({ text, setUserId, clearUser, player }) {
+  const [input, setInput] = useState("");
   return (
     <div id="player-select">
       <h2>{text}</h2>
-      <input
-        type="text"
-        onChange={(e) => {
-          setUserId(e.target.value);
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      <button
+        onClick={() => {
+          clearUser(player);
+          setUserId(input);
         }}
-      />
+      >
+        Click
+      </button>
     </div>
   );
 }
