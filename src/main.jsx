9869f3a1669ce2016714aaf4519ee74eca259fdc
Alex <atlexeide@gmail.com>
2025-08-14 14:38:00 +0200

    Add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/nuxt";
 import "./index.css";
 import App from "./App.jsx";
 import Header from "./components/Main/Header.jsx";
@@ -9,6 +10,7 @@ import Footer from "./components/Main/Footer.jsx";
 // </StrictMode>
 createRoot(document.getElementById("root")).render(
   <>
+    <Analytics />
     <Header />
     <App />
     <Footer />
