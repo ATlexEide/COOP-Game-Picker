4d7849b6fe8fb3bde6673394a5b7dc28d1db2b2f
Alex <atlexeide@gmail.com>
2025-08-14 11:26:31 +0200

    Fix import url

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Header from "./components/main/Header.jsx";
-import Footer from "./components/main/Footer.jsx";
+import Header from "./components/Main/Header.jsx";
+import Footer from "./components/Main/Footer.jsx";
 
 // <StrictMode>
 // </StrictMode>
