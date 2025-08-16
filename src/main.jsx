f8ffb49d2c3d2125a78abb9811e5d72de2a6c31e
Alexander Eide <atlexeide@gmail.com>
2025-08-16 14:00:17 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
+
 import "./index.css";
 import App from "./App.jsx";
 import Header from "./components/Main/Header.jsx";
 import Footer from "./components/Main/Footer.jsx";
 
-// <StrictMode>
-// </StrictMode>
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
