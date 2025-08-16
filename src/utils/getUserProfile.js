657a79fd1f3a23e6c1848a78f0c75220a06cb482
Alexander Eide <atlexeide@gmail.com>
2025-08-16 22:52:55 +0200

    Add "default" export

--- src/utils/getUserProfile.js
+++ src/utils/getUserProfile.js
@@ -1,4 +1,4 @@
-export async function getUserProfile(steamID, setUser) {
+export default async function getUserProfile(steamID, setUser) {
   await fetch(`${import.meta.env.VITE_SERVER_BASE_URL}/user/${steamID}/profile`)
     .then((res) => res.json())
     .then((res) => res.response.players[0])
