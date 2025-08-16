034ab08c5b5642dcf651f10400929461bf8d8852
Alexander Eide <atlexeide@gmail.com>
2025-08-16 22:48:36 +0200

    Update fetch urls

--- src/utils/getUserLibrary.js
+++ src/utils/getUserLibrary.js
@@ -1,6 +1,6 @@
 export async function getUserLibrary(steamID, setError) {
   return await fetch(
-    `${import.meta.env.VITE_SERVER_BASE_URL}/library/${steamID}`
+    `${import.meta.env.VITE_SERVER_BASE_URL}/user/${steamID}/library`
   )
     .then((res) => res.json())
     .then((res) => res.response.games)
--- src/utils/getUserProfile.js
+++ src/utils/getUserProfile.js
@@ -1,5 +1,5 @@
 export async function getUserProfile(steamID, setUser) {
-  await fetch(`${import.meta.env.VITE_SERVER_BASE_URL}/user/${steamID}`)
+  await fetch(`${import.meta.env.VITE_SERVER_BASE_URL}/user/${steamID}/profile`)
     .then((res) => res.json())
     .then((res) => res.response.players[0])
     .then((res) => {
