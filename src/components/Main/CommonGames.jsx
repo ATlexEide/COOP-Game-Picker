8197f78967301768a2d86f4ddf0c5306f55f44a8
Alex <atlexeide@gmail.com>
2025-08-20 12:49:49 +0200

    Update conditional rendering to account for random game

--- src/components/Main/CommonGames.jsx
+++ src/components/Main/CommonGames.jsx
@@ -1,19 +1,46 @@
+import { useState } from "react";
+import SelectedGame from "../SelectedGame";
 import "./CommonGames.css";
 
 export default function CommonGames({ games }) {
+  const [randomGame, setRandomGame] = useState(null);
+  function pickRandomGame() {
+    setRandomGame(games[Math.floor(Math.random() * games.length)]);
+  }
   console.log(games);
   return (
-    <section id="common-games">
-      <ul>
-        {games.map((game, i) => {
-          if (!game) return;
-          return (
-            <li className="game" key={i}>
-              <img src={game.capsule_image} alt={game.name + " thumbnail"} />
-            </li>
-          );
-        })}
-      </ul>
+    <section id={randomGame ? "common-games-random" : "common-games"}>
+      <div id="random-game-btns">
+        {randomGame && (
+          <button
+            onClick={() => {
+              setRandomGame(null);
+            }}
+          >
+            Back
+          </button>
+        )}
+        <button onClick={pickRandomGame}>
+          {randomGame ? "Try again" : "Pick radom game"}
+        </button>
+      </div>
+      {randomGame && (
+        <>
+          <SelectedGame game={randomGame} />
+        </>
+      )}
+      {!randomGame && (
+        <ul>
+          {games.map((game, i) => {
+            if (!game) return;
+            return (
+              <li className="game" key={i}>
+                <img src={game.capsule_image} alt={game.name + " thumbnail"} />
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </section>
   );
 }
